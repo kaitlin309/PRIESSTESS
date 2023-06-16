9a9e9d11228787f4d23ab83d4621b1e64e0e17da
Kaitlin Laverty <kaitlin309@gmail.com>
2023-06-16 11:59:16 -0400

    Update log2_scinot.py
    Fix for users that use commas instead of periods for decimal

--- bin/utils/log2_scinot.py
+++ bin/utils/log2_scinot.py
@@ -5,5 +5,8 @@ if __name__ == "__main__":
     # takes a number in the format 1.6e-300 or 4e3 and takes log2
     num=sys.argv[1]
     num_vec=num.split("e")
+    # For some users, numbers are in the format "1,6e-300" instead of "1.6e-300"
+    if "," in num_vec[0]:
+        num_vec[0] = num_vec[0].replace(",", ".")
     print(math.log(float(num_vec[0]), 2) + (float(num_vec[1])*math.log(10, 2)))
 
