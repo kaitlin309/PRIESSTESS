97cd81346f027b35a5ef456d05f0707bd54cd9c6
Kaitlin Laverty <kaitlin309@gmail.com>
2023-06-16 12:02:59 -0400

    Update log2_scinot.py
    Now handles numbers not in scientific notation

--- bin/utils/log2_scinot.py
+++ bin/utils/log2_scinot.py
@@ -3,10 +3,14 @@ import sys
 
 if __name__ == "__main__":
     # takes a number in the format 1.6e-300 or 4e3 and takes log2
+    # now also works if the number is not in scientific notation
     num=sys.argv[1]
-    num_vec=num.split("e")
     # For some users, numbers are in the format "1,6e-300" instead of "1.6e-300"
-    if "," in num_vec[0]:
-        num_vec[0] = num_vec[0].replace(",", ".")
-    print(math.log(float(num_vec[0]), 2) + (float(num_vec[1])*math.log(10, 2)))
+    if "," in num:
+        num = num.replace(",", ".")
+    if "e" in num:
+        num_vec=num.split("e")
+        print(math.log(float(num_vec[0]), 2) + (float(num_vec[1])*math.log(10, 2)))
+    else:
+        print(math.log(float(num), 2))
 
